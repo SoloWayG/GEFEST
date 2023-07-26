94f88ee6e91c24ba9ae9de54aa7cb916b467d1af
Gleb Solovev <glebsolo46@gmail.com>
2023-07-26 13:35:32 +0300

    update

--- gefest/tools/estimators/simulators/sound_wave/sound_interface.py
+++ gefest/tools/estimators/simulators/sound_wave/sound_interface.py
@@ -100,7 +100,7 @@ class SoundSimulator:
         self.domain = domain
         self.map_size = (round(1.2 * domain.max_y), round(1.2 * domain.max_x))
         self.size_y, self.size_x = self.map_size
-        self.duration = 250
+        self.duration = 300
         # obstacle_map handling
         if (
             obstacle_map is not None
